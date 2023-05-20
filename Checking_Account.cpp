31e52dee4e09ba412f84a50676974c9cbc422aaa
Kevin Koshy <kevin_koshy@hotmail.com>
2023-05-20 22:35:06 +0530

    Adding Checking Account functionality

---
+++ Checking_Account.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Kevin Koshy on 20/05/23.
+//
+
+#include "Checking_Account.h"
---
+++ Checking_Account.h
@@ -0,0 +1,14 @@
+//
+// Created by Kevin Koshy on 20/05/23.
+//
+
+#ifndef CHALLENGE_CHECKING_ACCOUNT_H
+#define CHALLENGE_CHECKING_ACCOUNT_H
+
+
+class Checking_Account {
+
+};
+
+
+#endif //CHALLENGE_CHECKING_ACCOUNT_H
